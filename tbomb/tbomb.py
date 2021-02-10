b9d6bb737beff296c1eaede3e36960eb89a69a92
J0k3R <49252733+esjoker@users.noreply.github.com>
2021-02-10 21:20:59 +0530

    Add files via upload

---
+++ tbomb/tbomb.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python2
+
+import os
+import ConfigParser
+import time
+from time import gmtime, strftime, sleep
+import lib
+import subprocess
+import readline
+class color:
+    HEADER = '\033[95m'
+    IMPORTANT = '\33[35m'
+    NOTICE = '\033[33m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RED = '\033[91m'
+    WHITE = '\033[37m'
+    END = '\033[0m'
+    UNDERLINE = '\033[4m'
+    LOGGING = '\33[34m'
+def logo():
+    print(color.RED+"       ..,;:ccccccc:;...")
+    print(color.WHITE+"    ..,clllc:;;;;;;:cllc,.")
+    print(color.RED+"   .,cllc,..............';;'.")
+    print(color.WHITE+"  .;lol;......"+color.WHITE+"_______"+color.RED+"....;lol;.")
+    print(color.RED+" .,lol;......"+color.WHITE+"/ _____/"+color.RED+".....;lol;..  ")
+    print(color.WHITE+" .coo......."+color.WHITE+"/ /"+color.RED+".............coo")
+    print(color.RED+".'lol,....."+color.WHITE+"/ /"+color.RED+"............'lol,.")
+    print(color.WHITE+".,lol,...."+color.WHITE+"/ /_____"+color.RED+"........,lol,.")
+    print(color.RED+".,lol,..."+color.WHITE+"/_______/"+color.RED+".......,lol,.")
+    print(color.WHITE+" .:ooc'.................:ooc'")
+    print(color.RED+"  .'cllc'.............cllc.")
+
+def clearScr():
+    os.system('clear')
+
+def yesOrNo():
+    return (raw_input("Continue Y / N: ") in yes)
+
+def return_fw():
+    bashCommand = ". ./cyanide-framework.sh && main"
+    output = subprocess.call(['bash','-c', bashCommand])
+
+installDir = os.path.dirname(os.path.abspath(__file__)) + '/'
+print(installDir)
+configFile = installDir + "/cyanide.cfg"
+print(installDir)
+config = ConfigParser.RawConfigParser()
+config.read(configFile)
+
+toolDir = installDir + config.get('cyanide', 'toolDir')
+logDir = installDir + config.get('cyanide', 'logDir')
+yes = config.get('cyanide', 'yes').split()
+
+color_random=[color.HEADER,color.IMPORTANT,color.NOTICE,color.OKBLUE,color.OKGREEN,color.WARNING,color.RED,color.END,color.UNDERLINE,color.LOGGING]
+# random.shuffle(color_random)
+continuePrompt = "\nClick [Return] to continue"
+alreadyInstalled = "Already Installed"
+
+class TBomb:
+    def __init__(self):
+        self.installDir = toolDir + "TBomb"
+        self.gitRepo = "https://github.com/TheSpeedX/TBomb.git"
+        # self.targetPrompt = color.RED + "\nLock Your Target " + color.WHITE + "IP/Subnet/Range/Host: "
+        if self.installed():
+            self.run()
+        else:
+            self.install()
+            self.run()
+
+    def installed(self):
+        return os.path.isfile(installDir + "tools/TBomb/TBomb.sh")
+
+    def install(self):
+        os.system("git clone --depth=1 %s %s" % (self.gitRepo, self.installDir))
+        os.system("cd %s && chmod 777 *.sh && sleep 1 && bash TBomb.sh" % self.installDir)
+
+    def run(self):
+        clearScr()
+        os.system("cd %s && bash TBomb.sh" % self.installDir)
+
+
+TBomb()
