163b1c133d9babc363fe07804a36071b919ececf
J0k3R <49252733+esjoker@users.noreply.github.com>
2021-02-10 21:37:23 +0530

    Delete md5generator.py

--- md5generator.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-plain_txt = input("Please enter the word you want to convert: ")
-
-#Converting your plain text into encoding (utf-8)
-text_enc = plain_txt.encode('utf-8')
-digest = hashlib.md5(text_enc.strip()).hexdigest()
-print("Your generated hash is :" + digest)
-
