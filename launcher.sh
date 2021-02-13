13cd7a5f316754cc38d28f167c95be6a8d9d7085
J0k3R <49252733+esjoker@users.noreply.github.com>
2021-02-13 20:51:06 +0530

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,20 @@
 
 [[ `id -u` -eq 0 ]] > /dev/null 2>&1 || { echo  $red "You must be root to run the script"; echo ; exit 1; }
 
+check= pip list | grep configparser | awk '{print$1}'
+sleep 0.5
+if check==configparser; then
+	echo installed
+else
+	pip install configparser  
+	pip2 install configparser
+
+fi
 
 clear
+
+
+
 
 pwd=$(pwd)
 
