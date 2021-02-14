73bd41ea8641c04ab540bd8c62fda132af7412d8
J0k3R <49252733+esjoker@users.noreply.github.com>
2021-02-14 18:20:23 +0530

    Update rot5_file.py

--- tola_atom/encoders/rot5_file.py
+++ tola_atom/encoders/rot5_file.py
@@ -1,4 +1,16 @@
-import codecs
-s = input("Enter a String: ")
-ro_encrypt = codecs.encode(s,"rot5")
-print("Ciphered Text: ",ro_encrypt)
+#!/usr/bin/env python2
+def rot5_cipher(message):
+	translated = ''
+	for character in message:
+		if character.isdigit():
+			character = (int(character)+5)% 10
+		translated = translated + str(character)
+	return translated
+
+# Encoding
+plaintext = raw_input("Enter the number to be ciphered : ")
+ciphertext=rot5_cipher(plaintext)
+print("\nYour Ciphered Text is : %s" %ciphertext)
+
+plaintext = ""
+plaintext = rot5_cipher(ciphertext) 
