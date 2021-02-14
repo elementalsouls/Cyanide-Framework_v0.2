8878dbd9a2a8a31ffcc44c1b9c416c3dccc9cbea
Dboidembla <51048430+Dboidembla@users.noreply.github.com>
2021-02-14 16:58:24 +0530

    Update md5_dec.py

--- tola_atom/decoders/md5_dec.py
+++ tola_atom/decoders/md5_dec.py
@@ -1,22 +1,41 @@
 import hashlib
 flag = 0
 pass_hash = input("Please Enter the hash you want to crack :")
-pass_file = input("enter the file name :")
-try:
-    pass_file = open(pass_file, "r")
-except:
-    print("No file found")
-    quit()
-for word in pass_file:
-    enc_word = word.encode('utf-8')
+optn = input("Do you want to Give a Custom Dictionary(Y/n)")
+if (optn=="Y") or (optn=="Yes") or (optn=="YES") or (optn=="yes"):
+    pass_file = input("enter the file name :")
+    try:
+        pass_file = open(pass_file, "r")
+    except:
+        print("No file found")
+        quit()
+    for word in pass_file:
+        enc_word = word.encode('utf-8')
 
-    digest = hashlib.md5(enc_word.strip()).hexdigest()
-    print(word)
-    print(digest)
-    print(pass_hash)
-    if digest == pass_hash:
-        print("Password has been found: " + word)
-        break
+        digest = hashlib.md5(enc_word.strip()).hexdigest()
+        print(word)
+        print(digest)
+        print(pass_hash)
+        if digest == pass_hash:
+            print("Password has been found: " + word)
+            break
+elif (optn=="N") or (optn=="No") or (optn=="n") or (optn=="no"):
+    pass_file = "/usr/share/wordlists/rockyou.txt"
+    try:
+        pass_file = open(pass_file, "r")
+    except:
+        print("No file found")
+        quit()
+    for word in pass_file:
+        enc_word = word.encode('utf-8')
+
+        digest = hashlib.md5(enc_word.strip()).hexdigest()
+        print(word)
+        print(digest)
+        print(pass_hash)
+        if digest == pass_hash:
+            print("Password has been found: " + word)
+            break
 
 if flag == 1:
     print("Password is not in the list")
