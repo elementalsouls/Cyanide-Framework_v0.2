05e77fef9bdddf5fce30b0053771224002ac662b
Dboidembla <51048430+Dboidembla@users.noreply.github.com>
2021-02-14 17:06:52 +0530

    Update sha384_dec.py

--- tola_atom/decoders/sha384_dec.py
+++ tola_atom/decoders/sha384_dec.py
@@ -1,29 +1,41 @@
 import hashlib
 flag = 0
 pass_hash = input("Please Enter the hash you want to crack :")
-pass_file = input("enter the file name :")
-#To open the file into read mode we use- Try and Except
-try:
-    pass_file = open(pass_file, "r")
-except:
-    print("No file found")
-    quit()
-for word in pass_file:
-    #when you are to convert any sti=ring in to hash, it requires certain encoding, which is "utf-8" here.
-    enc_word = word.encode('utf-8')
-    #create a hash digest coz hashing will create a hash object and that also has to be in some hex format.
-    #which is called hex digest in technical terms.
-    #we are using ".strip()" because of striping of any white spaces it might have.
-    digest = hashlib.sha256(enc_word.strip()).hexdigest()
-    print(word)
-    print(digest)
-    print(pass_hash)
-    if digest == pass_hash:
-        print("Password has been found: " + word)
-        break
-        #This actually handles if the password is in the list, what if the password is not in the list??
-        # To do this we will handle it using flag.
+optn = input("Do you want to Give a Custom Dictionary(Y/n)")
+if (optn=="Y") or (optn=="y") or (optn=="yes"):
+    pass_file = input("enter the file name :")
+    try:
+        pass_file = open(pass_file, "r")
+    except:
+        print("No file found")
+        quit()
+    for word in pass_file:
+        enc_word = word.encode('utf-8')
 
+        digest = hashlib.sha384(enc_word.strip()).hexdigest()
+        print(word)
+        print(digest)
+        print(pass_hash)
+        if digest == pass_hash:
+            print("Password has been found: " + word)
+            break
+elif (optn=="N") or (optn=="n") or (optn=="no"):
+    pass_file = "/usr/share/wordlists/rockyou.txt"
+    try:
+        pass_file = open(pass_file, "r")
+    except:
+        print("No file found")
+        quit()
+    for word in pass_file:
+        enc_word = word.encode('utf-8')
+
+        digest = hashlib.sha384(enc_word.strip()).hexdigest()
+        print(word)
+        print(digest)
+        print(pass_hash)
+        if digest == pass_hash:
+            print("Password has been found: " + word)
+            break
 
 if flag == 1:
     print("Password is not in the list")
